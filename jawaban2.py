88e267f0bff69f66938c32fdd0d01c27edfcd7e7
gilangm22 <114929578+gilangm22@users.noreply.github.com>
2023-01-23 17:14:02 +0700

    Delete jawaban2.py

--- jawaban2.py
+++
@@ -1,8 +0,0 @@
-print("PROGRAM PYTHON MENGHITUNG LUAS SEGITIGA")
-
-a = float(input("\nMasukan Alas  : "))
-t = float(input("Masukan Tinggi: "))
-
-luas = 0.5*a*t
-
-print("\nLuas Segitiga  = %0.2f" % luas)
