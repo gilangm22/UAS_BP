f75f392d85b5ff53de8db661d0eef46c005bd82c
gilangm22 <114929578+gilangm22@users.noreply.github.com>
2023-01-23 17:15:04 +0700

    Delete jawaban3.py

--- jawaban3.py
+++
@@ -1,9 +0,0 @@
-print("|============================|") 
-print("|       PROGRAM QUIS         |")
-print("|============================|")
-
-nama = str(input("Masukan Nama  = "))
-nim = str(input("Masukan NIM  = "))
-
-print("")
-print("")
